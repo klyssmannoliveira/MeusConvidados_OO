5139d36a0da2f87e73c8b6d7e317b9ce6ba28df3
klyssmannoliveira <56873266+klyssmannoliveira@users.noreply.github.com>
2021-03-19 23:37:00 -0300

    TP03: Criação da classe Musica
    Descrição: Classe que obtem os dados da Musica para a realização
    do evento.

---
+++ MeusConvidados_OO/src/Trabalho/Musica.java
@@ -0,0 +1,11 @@
+package Trabalho;
+
+public class Musica extends Gasto {
+	
+	// Construtor da classe musica
+	public Musica(String descricao, double valorTotal) {
+		super(descricao, valorTotal);
+		// TODO Auto-generated constructor stub
+	}
+
+}
