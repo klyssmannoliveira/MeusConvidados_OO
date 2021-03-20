84fc0bbe73fa67df2ed130cc6b37fc834bcd529b
HITER <HITER@DESKTOP-REMTFQR>
2021-03-20 00:46:34 -0300

    TP03: Atualização da classe Programaçao
    Descrição: método de atualizar seguindo a UML e comentários explicando
    cada método.

--- MeusConvidados_OO/src/Trabalho/Programacao.java
+++ MeusConvidados_OO/src/Trabalho/Programacao.java
@@ -1,25 +1,56 @@
 package Trabalho;
 
+/*
+Sintese
+Objetivo: Classe denominada Programação para descrever a programação do evento; 
+*/
+
 public class Programacao {
 
 	private String descricao; // Descrição da Programação
 
-	// Construtor 
+	/*
+	 * Objetivo: Construtor da Classe Programaçao; 
+	 * Entrada : descricao;
+	 * 
+	 */
+
 	public Programacao(String descricao) {
 		this.descricao = descricao;
 	}
 
-	
+	/*
+	 * Objetivo: Retorna a uma string; 
+	 * Saida: descrição;
+	 * 
+	 */
+
 	public String toString() {
 		return "A descrição da Programação do evento é:\n " + descricao;
 	}
 
-	// getters and setters da programação
-	
+	/*
+	 * Objetivo: Retorna a uma string;
+	 *  Entrada: descrição;
+	 * 
+	 */
+	public void atualizarProgramacao(String descricao) {
+
+		setDescricao(descricao);
+
+	}
+
+	/*
+	 * Objetivo: Getters e Setters referentes a Programação;
+	 * 
+	 */
+
+	// get da descrição da programação;
 	public String getDescricao() {
 		return descricao;
 	}
 
+	// set da descrição da programação;
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
