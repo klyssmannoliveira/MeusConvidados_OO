aff2a88c24c9446974e7bd70dc5875becef120c6
klyssmannoliveira <56873266+klyssmannoliveira@users.noreply.github.com>
2021-03-20 18:13:50 -0300

    TP03: Criação da classe orcamento
    Descrição: criação da nova classe orçamento, que servirá de pai para os
    gastos (bebida, comida, infraestrutura e musica). Também houve a
    exclusão da classe Gasto e outros

---
+++ MeusConvidados_OO/src/Trabalho/Orcamento.java
@@ -0,0 +1,46 @@
+package Trabalho;
+
+/*
+Sintese
+Objetivo: Classe denominada Orcamento para ser a classe Pai de todos os gastos do evento; 
+*/
+
+public class Orcamento {
+	private String descricao; // descricao do gasto
+	private double valorTotal; // valor total do gasto
+	
+	/*
+	 * Objetivo: Construtor da Classe Orcamento; 
+	 * Entrada : descricao, valorTotal;
+	 * 
+	 */
+	public Orcamento(String descricao, double valorTotal) {
+		this.descricao = descricao;
+		this.valorTotal = valorTotal;
+	}
+	
+	/*
+	 * Objetivo: Getters e Setters referentes ao evento;
+	 */
+	
+	// get da descricao
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	// set da descricao
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	// get do valor total
+	public double getValorTotal() {
+		return valorTotal;
+	}
+	
+	// set do valor total
+	public void setValorTotal(double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+}
