03c54a1819e61662fcb45a0a87615d642ab66694
klyssmannoliveira <56873266+klyssmannoliveira@users.noreply.github.com>
2021-03-20 16:02:46 -0300

    TP03: Criação da classe InformacaoConvidado.
    Descrição: classe InformacaoConvidado para as informações dos convidados
    do evento.

---
+++ MeusConvidados_OO/src/Trabalho/InformacaoConvidado.java
@@ -0,0 +1,47 @@
+package Trabalho;
+/*
+Sintese
+Objetivo: Classe denominada InformacaoConvidado para as informações dos convidados do evento; 
+*/
+
+public class InformacaoConvidado extends Informacao {
+	private String telefone; // telefone do convidado em string
+	private String email; // email do convidado em string
+	private String redesSociais; // rede social do convidado em string
+	
+	/*
+	 * Objetivo: Getters e Setters referentes aos atributos;
+	 */
+	// get do Telefone
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	// set do Telefone
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	// get do email
+	public String getEmail() {
+		return email;
+	}
+	
+	// set do email
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	// get da rede social
+	public String getRedesSociais() {
+		return redesSociais;
+	}
+	
+	// set da rede social
+	public void setRedesSociais(String redesSociais) {
+		this.redesSociais = redesSociais;
+	}
+	
+	
+
+}
