1fb3fa114c82e7b20a517b98ea9a4a34fd5b3859
HITER <HITER@DESKTOP-REMTFQR>
2021-03-20 17:27:22 -0300

    TP03: Atualização da classe Evento;
    Descrição: Inclusão dos métodos atualizar, comentários e inclusão da
    classe informacaoEvento;

--- MeusConvidados_OO/src/Trabalho/Evento.java
+++ MeusConvidados_OO/src/Trabalho/Evento.java
@@ -1,32 +1,45 @@
 package Trabalho;
 
+/*
+Sintese
+Objetivo: Classe denominada Evento para ser a classe central para organização do evento; 
+*/
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+
 
 public class Evento {
 
-	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy"); // instância o formato simples da data
-	private String nomeEvento; // nome do evento dando liberdade ao usuario para colocar qualquer nome
-	private Date dataEvento; // data do evento no formato Date
-	private int horaInicio; // hora de inicio Ex: 20
-	private int horaTermino; // hora do termino Ex: 23
-	private static ArrayList<Convidado> bdConvidados = new ArrayList<Convidado>(); // cria arraylist de convidados
-	private int totalConvidados = 0;
-	private static ArrayList<Tarefa> bdTarefas = new ArrayList<Tarefa>(); // cria arraylist de tarefas
-	private double taxaTarefas;
-	private static ArrayList<Gasto> bdGastos = new ArrayList<Gasto>(); // cria arraylist de Gastos
-	private double totalGasto = 0;
+	SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");// instância o formato simples da data;
+	private String nomeEvento; // nome do evento dando liberdade ao usuario para colocar qualquer nome;
+	private InformacaoEvento informacaoEvento = new InformacaoEvento(); // instanciação da classe informacaoEvento;
+	private static ArrayList<Convidado> bdConvidados = new ArrayList<Convidado>(); // cria arraylist de convidados;
+	private int totalConvidados = 0; // inicializaçao do atributo total de convidados;
+	private static ArrayList<Tarefa> bdTarefas = new ArrayList<Tarefa>(); // cria arraylist de tarefas;
+	private double taxaTarefas = 0.0; // inicializaçao do atributo taxa de tarefas;
+	private static ArrayList<Gasto> bdGastos = new ArrayList<Gasto>(); // cria arraylist de Gastos;
+	private double totalGasto = 0.0; // inicializaçao do atributo total de gastos;
 
-	// Construtor da classe Evento
-	public Evento(String nome, String data, int horai, int horaf) throws Exception {
+	/*
+	 * Objetivo: Construtor da Classe Evento; 
+	 * Entrada : nome, data, horai, horaf;
+	 * 
+	 */
+
+	public Evento(String nome, String data, int horai, int horaf, String endereco) throws Exception {
 		nomeEvento = nome;
-		dataEvento = formato.parse(data); // transforma a String em formato date
-		horaInicio = horai;
-		horaTermino = horaf;
+		informacaoEvento.setDataEvento(formato.parse(data)); // transforma a String em formato date;
+		informacaoEvento.setHoraInicio(horai);
+		informacaoEvento.setHoraTermino(horaf);
+		informacaoEvento.setEndereco(endereco);
 	}
 
-	// Listar convidados
+	/*
+	 * Objetivo: Mostrar as listas de convidados com sua respectivas quantidades de  acompanhantes;
+	 * 
+	 */
+
 	public void listarConvidados() {
 		System.out.print("*** Lista de convidados para o Evento: " + nomeEvento + " ***\n");
 		for (int i = 0; i < bdConvidados.size(); i++) {
@@ -36,17 +49,144 @@ public class Evento {
 		System.out.println("Total de convidados: " + getTotalConvidados() + "\n");
 	}
 
-	// Adicionar convidado
+	/*
+	 * Objetivo: Adicionar um convidado para o evento desejado;
+	 * 
+	 */
+
 	public void adicionarConvidado(Convidado convidado) {
 		bdConvidados.add(convidado);
 	}
 
-	// Atualizar Convidado
-	public void atualizarConvidados() {
+	/*
+	 * Objetivo: Atualiza o nome do convidado para o evento desejado;
+	 *  Entrada: nomeAntigo, nomeNovo;
+	 */
+
+	public void atualizarNomeConvidado(String nomeAntigo, String nomeNovo) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == nomeAntigo) {
+				aux.atualizarNome(nomeNovo);
+				System.out.println("Nome do convidado atualizado com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+			System.out.println("\"Nome do convidado não encontrado!!\n");
+
+		}
 
 	}
 
-	// Remover convidado
+	/*
+	 * Objetivo: Atualiza o telefone do convidado para o evento desejado; 
+	 * Entrada: nome e telefone;
+	 */
+
+	public void atualizarTelefoneConvidado(String nome, String telefone) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == nome) {
+				aux.atualizarTelefone(telefone);
+				System.out.println("Telefone do convidado atualizado com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+			System.out.println("\"Nome do convidado não encontrado!!\n");
+
+		}
+
+	}
+
+	/*
+	 * Objetivo: Atualiza a quantidade de acompanhantes do convidado para o evento desejado; 
+	 * Entrada: nome e quantAcompanhante;
+	 */
+
+	public void atualizarQuantAcompanhanteConvidado(String nome, String quantAcompanhante) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == nome) {
+				aux.atualizarTelefone(quantAcompanhante);
+				System.out.println("Quantidade de acompanhates do convidado atualizado com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+			System.out.println("\"Nome do convidado não encontrado!!\n");
+
+		}
+
+	}
+
+	/*
+	 * Objetivo: Atualiza o email do convidado para o evento desejado; 
+	 * Entrada: nome e email;
+	 */
+
+	public void atualizarEmailConvidado(String nome, String email) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == nome) {
+				aux.atualizarTelefone(email);
+				System.out.println("Email do convidado atualizado com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+			System.out.println("\"Nome do convidado não encontrado!!\n");
+
+		}
+
+	}
+
+	/*
+	 * Objetivo: Atualiza a rede social do convidado para o evento desejado;
+	 * Entrada: nome e redeSocial;
+	 */
+
+	public void atualizarRedeSocialConvidado(String nome, String redeSocial) {
+		int cont = 0;
+		for (int i = 0; i < bdConvidados.size(); i++) {
+			Convidado aux = bdConvidados.get(i);
+			if (aux.getNome() == nome) {
+				aux.atualizarTelefone(redeSocial);
+				System.out.println("Rede Social do convidado atualizado com sucesso!!\n");
+				cont--;
+				break;
+			}
+			cont++;
+		}
+
+		if (cont == bdConvidados.size()) {
+			System.out.println("\"Nome do convidado não encontrado!!\n");
+
+		}
+
+	}
+
+	/*
+	 * Objetivo: Remove o convidado do evento desejado; 
+	 * Entrada: Convidado;
+	 */
 
 	public void removerConvidado(String convidado) {
 		int cont = 0;
@@ -69,7 +209,9 @@ public class Evento {
 
 	}
 
-	// Calculo do total de convidados
+	/*
+	 * Objetivo: Cálculo total dos convidados;
+	 */
 	public void setTotalConvidados() {
 		totalConvidados = 0;
 		for (int i = 0; i < bdConvidados.size(); i++) {
@@ -78,7 +220,9 @@ public class Evento {
 		}
 	}
 
-	//// Listar tarefa
+	/*
+	 * Objetivo: listar as tarefas realizadas para o evento;
+	 */
 	public void listarTarefas() {
 		int quantidade = 0;
 
@@ -93,13 +237,18 @@ public class Evento {
 		System.out.printf("Taxa de tarefas concluídas:  %.2f%%\n\n", getTaxaTarefas());
 	}
 
-	// Adicionar Tarefa
+	/*
+	 * Objetivo: Adicionar tarefas para ser realizadas no evento;
+	 *  Entrada: tarefa;
+	 */
 	public void adicionarTarefa(Tarefa tarefa) {
 		bdTarefas.add(tarefa);
 	}
 
-	// Remover Tarefa
-
+	/*
+	 * Objetivo: Remover tarefas para não ser realizadas no evento; 
+	 * Entrada: descricao;
+	 */
 	public void removerTarefa(String descricao) {
 		int cont = 0;
 		for (int i = 0; i < bdTarefas.size(); i++) {
@@ -121,7 +270,9 @@ public class Evento {
 
 	}
 
-	// Calculo da taxa de tarefas
+	/*
+	 * Objetivo: Calcular as taxas de tarefas do evento. Ex: 100% das tarefas concluidas;
+	 */
 	public void setTaxaTarefas() {
 		double contadorSIM = 0;
 		int cont = 0;
@@ -135,7 +286,9 @@ public class Evento {
 		taxaTarefas = (contadorSIM / cont) * 100;
 	}
 
-	// Listar Gastos
+	/*
+	 * Objetivo: Listar os gastos do evento;
+	 */
 	public void listarGastos() {
 		System.out.printf("*** Lista de gastos para o Evento: " + nomeEvento + " ***");
 		for (int i = 0; i < bdGastos.size(); i++) {
@@ -146,12 +299,18 @@ public class Evento {
 		System.out.printf("\nTotal de gastos: %.2f\n", getTotalGasto());
 	}
 
-	// Adicionar Gasto
-	public void adicionarGasto(Gasto convidado) {
-		bdGastos.add(convidado);
+	/*
+	 * Objetivo: Adicionar gastos do evento; 
+	 * Entrada: gasto;
+	 */
+	public void adicionarGasto(Gasto gasto) {
+		bdGastos.add(gasto);
 	}
 
-	// Remover Gastos
+	/*
+	 * Objetivo:Remover gastos do evento; 
+	 * Entrada: descricao;
+	 */
 	public void removerGasto(String descricao) {
 		int cont = 0;
 		for (int i = 0; i < bdGastos.size(); i++) {
@@ -172,7 +331,10 @@ public class Evento {
 
 	}
 
-	// Calculo do total de gastos
+	/*
+	 * Objetivo: Cálculo total dos gastos do evento;
+	 */
+
 	public void setTotalGasto() {
 		totalGasto = 0;
 		for (int i = 0; i < bdGastos.size(); i++) {
@@ -181,61 +343,45 @@ public class Evento {
 		}
 	}
 
-
-	// Relatório 
-	
+	/*
+	 * Objetivo: Relatório do evento que lista os convidados, as tarefas e os gastos;
+	 */
 	public void relatorio() {
-		
+
 		System.out.println("Relatório do evento");
 		listarConvidados();
 		listarTarefas();
 		listarGastos();
 
 	}
-	
-	// Getters e setters
+
+	/*
+	 * Objetivo: Getters e Setters referentes ao evento;
+	 * 
+	 */
+	// get nome do evento;
 	public String getNomeEvento() {
 		return nomeEvento;
 	}
 
+	// set do nome do evento;
 	public void setNomeEvento(String nomeEvento) {
 		this.nomeEvento = nomeEvento;
 	}
 
-	public Date getDataEvento() {
-		return dataEvento;
-	}
-
-	public void setDataEvento(Date dataEvento) {
-		this.dataEvento = dataEvento;
-	}
-
-	public int getHoraInicio() {
-		return horaInicio;
-	}
-
-	public void setHoraInicio(int horaInicio) {
-		this.horaInicio = horaInicio;
-	}
-
-	public int getHoraTermino() {
-		return horaTermino;
-	}
-
-	public void setHoraTermino(int horaTermino) {
-		this.horaTermino = horaTermino;
-	}
-
+	// get do total de convidados do evento;
 	public int getTotalConvidados() {
 		setTotalConvidados();
 		return totalConvidados;
 	}
 
+	// get da taxa de tarefas do evento;
 	public double getTaxaTarefas() {
 		setTaxaTarefas();
 		return taxaTarefas;
 	}
 
+	// get do total de de gastos do evento;
 	public double getTotalGasto() {
 		setTotalGasto();
 		return totalGasto;
