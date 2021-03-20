030c0cfd099180a1b2a08db6d19cf6e81239eec9
HITER <HITER@DESKTOP-REMTFQR>
2021-03-19 23:33:50 -0300

    TP03: Criação da classe Infraestrutura
    Descrição: Classe que obtem os dados da infraestrutura para a realização
    do evento.

---
+++ MeusConvidados_OO/src/Trabalho/Infraestrutura.java
@@ -0,0 +1,11 @@
+package Trabalho;
+
+public class Infraestrutura extends Gasto { // Infraestrutura compreende todos os gastos no local
+	
+	// Construtor de infraestrutura
+	public Infraestrutura(String descricao, double valorTotal) {
+		super(descricao, valorTotal);
+		// TODO Auto-generated constructor stub
+	}
+
+}
