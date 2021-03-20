5537d3a869166b807468f077604b322237b5d386
HITER <HITER@DESKTOP-REMTFQR>
2021-03-19 23:27:06 -0300

    TP03: Criação da classe Bebida
    Descrição: Classe que obtem os dados das bebidas para a realização do
    evento.

---
+++ MeusConvidados_OO/src/Trabalho/Bebida.java
@@ -0,0 +1,40 @@
+package Trabalho;
+
+public class Bebida extends Gasto {
+	private int quantidade;
+	private double valorUnitario;
+	
+	// Construtor de bebida
+	public Bebida(String descricao, double valorTotal) { // Construtor de gasto
+		super(descricao, valorTotal);
+	}
+	// Sobracarga do construtor de bebida
+	public Bebida(String descricao, double valorTotal, int quant) {
+		super(descricao, valorTotal);
+		quantidade = quant;
+		valorUnitario = valorTotal / (double) quant;
+	}
+
+	public String toString() {
+		return "Gasto: " + descricao + "\nQuantidade: " + quantidade + "\nValor unitário: R$ " + valorUnitario
+				+ "\nValor total: R$ " + valorTotal;
+	}
+
+	// getters e setters
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(double valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+}
