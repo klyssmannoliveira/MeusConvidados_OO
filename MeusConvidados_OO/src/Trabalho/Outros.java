5525d2efdc828f316d7536a2d58cd20480b5821d
HITER <HITER@DESKTOP-REMTFQR>
2021-03-19 23:42:05 -0300

    TP03: Criação da classe Outros
    Descrição: Classe que obtem os dados de outro tipo de gastos para
    realização do
    evento.

---
+++ MeusConvidados_OO/src/Trabalho/Outros.java
@@ -0,0 +1,11 @@
+package Trabalho;
+
+public class Outros extends Gasto {
+	
+	// Construtor da classe outros
+	public Outros(String descricao, double valorTotal) {
+		super(descricao, valorTotal);
+		// TODO Auto-generated constructor stub
+	}
+
+}
